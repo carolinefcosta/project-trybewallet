c41d7e80123cb682f1b92755172c68a58978864e
Caroline <carolinefernandesc@hotmail.com>
2023-03-09 19:55:50 -0300

    requisito 03 iniciando a lógica

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <section>
+        <label htmlFor="value-input">
+          <input data-testid="value-input" id="value-input" />
+        </label>
+        <textarea data-testid="description-input" />
+        <select data-testid="currency-input">
+          <option>a</option>
+        </select>
+      </section>
     );
   }
 }
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,27 @@
 export const SAVED_EMAIL = 'SAVED_EMAIL';
+export const WAITING_API = 'WAITING_API';
+export const API_SUCCESS = 'API_SUCCESS';
 
 const savedEmail = (email) => ({
   type: SAVED_EMAIL,
   payload: email,
 });
+
+const waitingApi = () => ({
+  type: 'WAITING_API',
+});
+
+const apiSuccess = (data) => ({
+  type: 'API_SUCCESS',
+  payload: data,
+});
+
+export const fetchApiCoins = () => async (dispatch) => {
+  dispatch(waitingApi());
+  const response = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const result = await response.json();
+  dispatch(apiSuccess(result));
+  console.log(result);
+};
 
 export default savedEmail;
